816e577a722b772616029ec8355fa0ff57b8c5c8
pomka <kolomnikov0405@gmail.com>
2024-07-19 14:16:41 +0700

    test

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -12,7 +12,7 @@ const UploadPage = () => {
   useEffect(() => {
     const fetchFiles = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/files');
+        const response = await axios.get('https://server-pleer.onrender.com:3000/files');
         setFiles(response.data);
       } catch (error) {
         console.error('Error fetching files:', error);
