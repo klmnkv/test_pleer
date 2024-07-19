77bcbc77ce40700bef5ceb56b6e7c24781ef3672
pomka <kolomnikov0405@gmail.com>
2024-07-19 16:35:42 +0700

    nice try

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import AudioPlayer from './AudioPlayer';
 
 const PlayerPage = () => {
   const location = useLocation();
-  const urlParams = new URLSearchParams(location.search);
-  const audioUrl = urlParams.get('url');
+  const params = new URLSearchParams(location.search);
+  const audioUrl = decodeURIComponent(params.get('url'));
 
   return (
-    <div>
-      <AudioPlayer audioUrl={audioUrl} />
+    <div className="container">
+      <h2>Audio Player</h2>
+      {audioUrl ? (
+        <audio controls src={audioUrl} aria-label="Audio player for the selected file" />
+      ) : (
+        <p>No audio file selected</p>
+      )}
     </div>
   );
 };
